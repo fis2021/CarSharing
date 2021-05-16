4c45832fd6efe111d656327d2a7dd16803806747
sorinburciu <sorin.burciu13@gmail.com>
2021-05-16 09:32:00 +0300

    "CAR-7" Am realizat conexiunea dintre proiect si baza de date create

---
+++ src/main/java/database/Configs.java
@@ -0,0 +1,9 @@
+package database;
+
+public class Configs {
+    protected String dbHost = "localgost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "sorinburciu";
+    protected String dbName = "users";
+}
---
+++ src/main/java/database/Const.java
@@ -0,0 +1,10 @@
+package database;
+
+public class Const {
+    public static final String USER_TABLE = "usersadd";
+
+    public static final String USERS_ID = "idusersAdd";
+    public static final String USERS_NAME = "username";
+    public static final String USERS_PASSWORD = "password";
+    public static final String USERS_NUMBER = "number";
+}
